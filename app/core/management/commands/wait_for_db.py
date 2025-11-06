e70ba8e1a4db47bb488528399d7a7ae4f6053cad
mikiyasalemayehu-mekonen <mikiyealem@gmail.com>
2025-11-06 13:48:08 +0300

    feat:created a super admin

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -4,22 +4,29 @@ Django command to wait for the database to be available.
 import time
 from psycopg2 import OperationalError as Psycopg2Error
 from django.db.utils import OperationalError
-
-from django.core.management.base import BaseCommand 
+from django.core.management.base import BaseCommand
 
 
 class Command(BaseCommand):
+    """Django command to pause execution until the database is available."""
 
     def handle(self, *args, **options):
         self.stdout.write("Waiting for database...")
         db_up = False
-        while not db_up:
-            try:  
-                self.check(databases=['default'])
+        retries = 0
+        max_retries = 60
+
+        while not db_up and retries < max_retries:
+            try:
+                self.check(databases=["default"])
                 db_up = True
             except (Psycopg2Error, OperationalError):
-                self.stdout.write("Database unavailable, waiting...")
+                retries += 1
+                self.stdout.write(f"Database unavailable, waiting... ({retries}/{max_retries})")
                 time.sleep(1)
-        self.stdout.write(self.style.SUCCESS("Database available!"))
 
-    
+        if not db_up:
+            self.stdout.write(self.style.ERROR("Database not available after 30 seconds."))
+            exit(1)
+        else:
+            self.stdout.write(self.style.SUCCESS("Database available!"))
